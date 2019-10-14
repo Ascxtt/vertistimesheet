b2c7d4bd74bd9796f20c5b817792b0229b21a8ec
Romaine Ascott <romaine.ascott@gmail.com>
2019-10-14 16:22:05 -0500

    bug fix

--- app.js
+++ app.js
@@ -111,30 +111,30 @@ app.use((req, res, next) => {
 
 //create user
      
-app.use((req, res, next) => {
-  const user = new User({
-    firstName: "Romaine",
-    lastName: "Ascott",
-    email: "romaine.ascott@gmail.com",
-    password: "123456"
-  });
+// app.use((req, res, next) => {
+//   const user = new User({
+//     firstName: "Romaine",
+//     lastName: "Ascott",
+//     email: "romaine.ascott@gmail.com",
+//     password: "123456"
+//   });
 
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) throw err;
-      user.password = hash;
-      user
-        .save()
-        .then(user => {
-          console.log(user);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    });
-  });
-  next();
-});
+//   bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(user.password, salt, (err, hash) => {
+//       if (err) throw err;
+//       user.password = hash;
+//       user
+//         .save()
+//         .then(user => {
+//           console.log(user);
+//         })
+//         .catch(err => {
+//           console.log(err);
+//         });
+//     });
+//   });
+//   next();
+// });
 
 // global variables
 app.use(function(req, res, next) {
